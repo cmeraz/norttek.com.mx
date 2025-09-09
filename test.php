2df08ccd23778c2dc5e4315a7ff6e8572f0f2f03
Carlos Meraz <cmeraz3944@gmail.com>
2025-09-08 22:49:47 -0600

    Update navbar.php
    Cambios en el menu para dejar poner un sidebar y reacomodar el de escritorio

---
+++ test.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php include('includes/header.php'); ?>
+</head>
+<body>
+
+    <?php 
+
+        include 'includes/navbar.php'; 
+        
+    ?>
+
+</body>
+</html>
