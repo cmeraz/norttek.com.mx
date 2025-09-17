af23154e2d29b6835b2d4acd6bef687d00fa219f
Carlos Meraz <cmeraz3944@gmail.com>
2025-09-17 13:20:30 -0600

    Create soporte.php
    pagina php que envia a descargar RustDesk de manera automatica con una redireccion JS

---
+++ soporte.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RustDesk - Escritorio remoto de código abierto</title>
+
+    <!-- Meta descripción y SEO -->
+    <meta name="description" content="RustDesk ofrece una solución de escritorio remoto de código abierto con opciones de servidor autohospedado. La alternativa perfecta a TeamViewer para un acceso remoto seguro, privado y personalizable. Explore nuestras licencias profesionales locales.">
+    <meta name="keywords" content="software escritorio remoto, acceso remoto código abierto, escritorio remoto autoalojado, alternativa TeamViewer, alternativa AnyDesk, control remoto seguro, acceso remoto multiplataforma, escritorio remoto local, servidor rustdesk">
+    <meta name="robots" content="index,follow">
+    <link rel="canonical" href="https://rustdesk.com/es">
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:title" content="RustDesk: Escritorio remoto de código abierto con soluciones de servidor autoalojado">
+    <meta property="og:description" content="RustDesk ofrece una solución de escritorio remoto de código abierto con opciones de servidor autohospedado. La alternativa perfecta a TeamViewer para un acceso remoto seguro, privado y personalizable. Explore nuestras licencias profesionales locales.">
+    <meta property="og:url" content="https://rustdesk.com/es">
+    <meta property="og:type" content="website">
+    <meta property="og:image" content="https://rustdesk.com/_astro/default.BnGe5sEj_Mm9fb.webp">
+    <meta property="og:locale" content="es_ES">
+    <meta property="og:site_name" content="RustDesk">
+
+    <!-- Twitter Card -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@rustdesk">
+    <meta name="twitter:creator" content="@rustdesk">
+</head>
+<body>
+    <script>
+        // Redirige y no permite volver atrás
+        window.location.replace("https://github.com/rustdesk/rustdesk/releases/download/1.4.2/rustdesk-1.4.2-x86_64.exe");
+    </script>
+</body>
+</html>
