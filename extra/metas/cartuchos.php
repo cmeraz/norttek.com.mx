800023b3dd437333b0d1ca990fef3bd62c197623
Carlos Meraz <cmeraz3944@gmail.com>
2025-09-18 17:44:40 -0600

    Create cartuchos.php
    Se creo el archivo extra/metas/cartuchos.php para controlar el seo de la pagina

---
+++ extra/metas/cartuchos.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * extra/metas/index.php
+ * Metas SEO y Open Graph específicas para la página de inicio
+ */
+
+$seo = [
+    // SEO básico
+    'title'       => 'Norttek Solutions - Seguridad Integral para tu Hogar y Oficina',
+    'description' => 'Norttek Solutions ofrece instalación profesional de CCTV, alarmas inteligentes, control de acceso, redes y cableado estructurado.',
+    'keywords'    => 'Seguridad, CCTV, Alarmas, Control de acceso, Redes, Automatización, Norttek, Oficina, Hogar, Empresas',
+    'robots'      => 'index, follow',
+
+    // Open Graph
+    'og_title'       => 'Norttek Solutions - Inicio',
+    'og_description' => 'Protege tu hogar y empresa con soluciones integrales de seguridad de Norttek.',
+    'og_url'         => 'https://www.norttek.com.mx/',
+    'og_image'       => 'https://www.norttek.com.mx/assets/images/og-image.jpg',
+
+    // Twitter Card
+    'twitter_title'       => 'Norttek Solutions - Inicio',
+    'twitter_description' => 'Protege tu hogar y empresa con soluciones integrales de seguridad de Norttek.',
+    'twitter_image'       => 'https://www.norttek.com.mx/assets/images/og-image.jpg'
+];
